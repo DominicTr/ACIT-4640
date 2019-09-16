2fe35fe754a2b37d7551bea7d0594040f9164f81
DominicTr <35462804+DominicTr@users.noreply.github.com>
2019-09-16 14:10:36 -0700

    Changed file name

--- host_network_setup.sh
+++ host_network_setup.sh
@@ -13,8 +13,8 @@ vbmg createvm --name VM_ACIT4640 --ostype RedHat_64 --register
 vbmg modifyvm VM_ACIT4640 --cpus 1 --memory 1024 --nic1 natnetwork --nat-network1 net_4640 --audio none
 
 
-vbmg createmedium disk --filename TEST --size 10000 --format VDI
+vbmg createmedium disk --filename VM-4640 --size 10000 --format VDI
 vbmg storagectl VM_ACIT4640 --name SATA --add sata --portcount 2 --bootable on
-vbmg storageattach VM_ACIT4640 --storagectl SATA --device 0 --port 0 --type hdd --medium "TEST.vdi"
+vbmg storageattach VM_ACIT4640 --storagectl SATA --device 0 --port 0 --type hdd --medium "VM-4640.vdi"
 vbmg storagectl VM_ACIT4640 --name IDE --add ide
 vbmg storageattach VM_ACIT4640 --storagectl IDE --device 0 --port 0 --type hdd --medium emptydrive
