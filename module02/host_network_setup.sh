34fd9cfbca85425396c783c9fb97ad8700a36736
DominicTr <35462804+DominicTr@users.noreply.github.com>
2019-09-17 13:47:19 -0700

    update file path

--- module02/host_network_setup.sh
+++ module02/host_network_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#Dominic Triwiang
-
 export PATH=/mnt/e/VirtualBox:$PATH
 vbmg () { ./VBoxManage.exe "$@"; }
 
@@ -13,10 +11,15 @@ vbmg natnetwork add --netname net_4640 --dhcp off --ipv6 off --network 192.168.2
 
 vbmg createvm --name VM_ACIT4640 --ostype RedHat_64 --register
 vbmg modifyvm VM_ACIT4640 --cpus 1 --memory 1024 --nic1 natnetwork --nat-network1 net_4640 --audio none
+VM_NAME="VM_ACIT4640"
+SED_PROGRAM="/^Config file:/ { s/^.*:\s\+\(\S\+\)/\1/; s|\\\\|/|gp }"
+VBOX_FILE=$(vbmg showvminfo "$VM_NAME" | sed -ne "$SED_PROGRAM")
+VM_DIR=$(dirname "$VBOX_FILE")
+echo $VM_DIR
 
 
-vbmg createmedium disk --filename VM-4640 --size 10000 --format VDI
+vbmg createmedium disk --filename "${VM_DIR}/VM-4640" --size 10000 --format VDI
 vbmg storagectl VM_ACIT4640 --name SATA --add sata --portcount 2 --bootable on
-vbmg storageattach VM_ACIT4640 --storagectl SATA --device 0 --port 0 --type hdd --medium "VM-4640.vdi"
+vbmg storageattach VM_ACIT4640 --storagectl SATA --device 0 --port 0 --type hdd --medium "${VM_DIR}/VM-4640.vdi"
 vbmg storagectl VM_ACIT4640 --name IDE --add ide
 vbmg storageattach VM_ACIT4640 --storagectl IDE --device 0 --port 0 --type hdd --medium emptydrive
